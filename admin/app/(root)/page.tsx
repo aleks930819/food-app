6f7d307b1260006292f42440f658e733b7d643fc
Aleksandar Atanasov <aleks930819@gmail.com>
2023-10-29 10:36:04 +0200

    test

--- admin/app/(root)/page.tsx
+++ admin/app/(root)/page.tsx
@@ -3,8 +3,6 @@ import { useEffect } from 'react';
 
 import { useStoreModal } from '@/hooks/use-store-modal';
 
-import { Modal } from '@/components/ui/modal';
-
 const RootPage = () => {
   const isOpen = useStoreModal((state) => state.isOpen);
   const onOpen = useStoreModal((state) => state.onOpen);
